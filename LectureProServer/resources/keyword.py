2db369fb90f62d9f25ffe674d618c29f6bab82f0
Phil <baldonp1@tcnj.edu>
2018-06-07 21:29:15 -0400

    Keyword extractor parses all sentences.

--- LectureProServer/resources/keyword.py
+++ LectureProServer/resources/keyword.py
@@ -12,13 +12,23 @@ class Keyword(Resource):
                 data = noteFileHandler.read()
                 # Create new ExtractorDelegator to handle keyword extractor
                 # extractor_delegator = keyword_extractor
-                keyword = extractPhrases(data)
+                results = extractPhrases(data)
 
                 with open("data/keywords/" + file[:15] + "_notes.txt", "w") as keywordFileHandler:
-                    keywordFileHandler.write("Full sentence:{}\n\nKey phrases{}".format(keyword[0], keyword[1]))
+                    all_keywords = []
+
+                    for keyword in results:
+                        all_keywords.append(keyword[1])
+                        keywordFileHandler.write("Full sentence:{}\nKey phrases{}\n\n".format(keyword[0], keyword[1]))
+
 
                 app.logger.info(file[:15] + "_notes.txt file saved successfully.")
-                return keyword
+                print("done")
+                all_keywords = [x for sublist in all_keywords for x in sublist]
+
+                return results
+
+                #return [data, all_keywords[:]]
         except OSError as err:
             app.logger.info("OS error: {0}".format(err))
             return "Audio to text transcribe failed!", 422
--- LectureProServer/resources/search.py
+++ LectureProServer/resources/search.py
@@ -12,35 +12,34 @@ class Search(Resource):
     def get(self, query):
         app.logger.info(query)
 
-        query_keywords = extractPhrases(query)[1]
+        test = extractPhrases(query)
+        print(test)
+        query_keywords = [x[1] for x in test]
+        print(query_keywords)
+        query_keywords = [x for sublist in query_keywords for x in sublist]
         query_keywords = [x.lower().split() for x in query_keywords]
         query_keywords = reduce(operator.concat, query_keywords)
 
         results = []
-
-        print(query_keywords)
 
         for notes_file in os.listdir("data/notes/"):
             if notes_file.endswith(".txt"):
                 print(notes_file)
                 with open("data/notes/" + notes_file, "r") as noteFileHandler:
                     data = noteFileHandler.read()
-                    note_keywords = extractPhrases(data)[1]
-                    print(note_keywords)
+                    note_keywords = [x[1] for x in extractPhrases(data)]
+                    note_keywords = [x for sublist in note_keywords for x in sublist]
                     note_keywords = [x.lower() for x in note_keywords]
 
                 file_hits=0
                 for query_keyword in query_keywords:
                     for note_keyword in note_keywords:
-                        print(query_keyword)
                         if query_keyword in note_keyword:
                             print("hit")
                             file_hits += 1
 
                 if file_hits > 0:
                     results.append([notes_file, file_hits])
-
-
 
         results = sorted(results, key=itemgetter(1), reverse=True)
         ordered_files = [x[0] for x in results]
