1ba5d21e4c5878316f139c93192bb10e8a3f9646
Phil <baldonp1@tcnj.edu>
2018-06-07 21:31:56 -0400

    Cleaned up test code.

--- LectureProServer/resources/keyword.py
+++ LectureProServer/resources/keyword.py
@@ -13,22 +13,15 @@ class Keyword(Resource):
                 # Create new ExtractorDelegator to handle keyword extractor
                 # extractor_delegator = keyword_extractor
                 results = extractPhrases(data)
-
                 with open("data/keywords/" + file[:15] + "_notes.txt", "w") as keywordFileHandler:
-                    all_keywords = []
-
                     for keyword in results:
-                        all_keywords.append(keyword[1])
                         keywordFileHandler.write("Full sentence:{}\nKey phrases{}\n\n".format(keyword[0], keyword[1]))
 
 
                 app.logger.info(file[:15] + "_notes.txt file saved successfully.")
-                print("done")
-                all_keywords = [x for sublist in all_keywords for x in sublist]
 
                 return results
 
-                #return [data, all_keywords[:]]
         except OSError as err:
             app.logger.info("OS error: {0}".format(err))
             return "Audio to text transcribe failed!", 422
